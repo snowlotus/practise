d18861e92e77af07499760224c3011853242300e
snowdrop <dongandhuang@gmail.com>
2018-05-02 16:50:25 +0800

    todo list with vue-cli

---
+++ todolist/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <input  v-model="inputValue" />
+    <button class="item" @click="handleSubmit">submit</button>
+
+    <ul>
+      <todo-item 
+        v-for="(item,index) in list" 
+        :key="index"
+        :content="item" 
+        :index="index"
+        @delete="handleDelete"
+        ></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import TodoItem from './components/TodoItem';
+  export default {
+    components:{
+      'todo-item':TodoItem
+    },
+    data(){
+      return {
+        inputValue:'',
+        list:[]
+      }
+    },
+    methods:{
+      handleSubmit(){
+        this.list.push(this.inputValue);
+        this.inputValue='';
+      },
+      handleDelete(index){
+        this.list.splice(index,1);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
